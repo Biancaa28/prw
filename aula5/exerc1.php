f5743ebf333af5e6299293eeb92064f6cff13d9b
Bianca <bianca.chioderoli@aluno.ifsp.edu.br>
2022-03-23 11:42:30 -0300

    exemplo 3

---
+++ aula5/exerc1.php
@@ -0,0 +1,45 @@
+<?php
+$n1 = 4;
+$n2 = 3;
+$n3 = 1;
+
+if($n1 > $n2 && $n1 > $n3)
+{
+    if($n2 > $n3)
+    {
+        echo "Ordem decrescente: ". $n1. $n2. $n3;
+    }
+    else
+    {
+        echo "Ordem decrescente: ". $n1. $n3. $n2;
+    }
+}
+else
+{
+    if($n2 > $n1 && $n2 > $n3)
+    {
+        if($n1 > $n3)
+        {
+            echo "Ordem decrescente: ". $n2. $n1. $n3;
+        }
+        else
+        {
+            echo "Ordem decrescente: ". $n2. $n3. $n1;
+        }
+    }
+    else
+    {
+        if($n3 > $n1 && $n3 > $n2)
+        {
+            if($n1 > $n2)
+            {
+                echo "Ordem decrescente: ". $n3. $n1. $n2;
+            }
+            else
+            {
+                echo "Ordem decrescente: ". $n3. $n2. $n1;
+            }
+        }
+    }
+}
+?>
